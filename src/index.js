ab10c0e58c550fc9a211673355a8415d127c34c6
Max Holzheu <maxhm10@gmail.com>
2021-05-05 07:32:59 -0600

    prepare for release

---
+++ src/index.js
@@ -0,0 +1,61 @@
+class Recurrente {
+  constructor(key) {
+    this.publicKey = key
+    this.setup()
+  }
+
+  static init(key) {
+    return new Recurrente(key)
+  }
+
+  createCheckout(button) {
+    const outerThis = this
+    fetch(`http://localhost:3001/api/checkouts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-PUBLIC-KEY": this.publicKey,
+      }, body: JSON.stringify({
+        items: [{
+          price_id: this._priceId(button),
+          quantity: this._quantity(button),
+        }],
+      })
+    }).then(function (result) {
+      return result.json();
+    }).then(function (data) {
+      if (data.checkout_url) {
+        outerThis.redirectToCheckout(data.checkout_url)
+      } else {
+        console.log(data)
+      }
+    })
+  }
+
+  _priceId(button) {
+    return button.getAttribute("data-recurrente-price-id") || this.priceId
+  }
+
+  _quantity(button) {
+    return button.getAttribute("data-recurrente-quantity") || this.quantity || 1
+  }
+
+  redirectToCheckout(url) {
+    window.location = url
+  }
+
+  setup() {
+    const outerThis = this
+    const submitButtons = Array.from(document.querySelectorAll("[data-recurrente-button]"))
+    if (submitButtons.length > 0) {
+      submitButtons.map((submitButton) => {
+        submitButton.addEventListener("click", function () {
+          outerThis.createCheckout(submitButton)
+        })
+      })
+    }
+  }
+}
+
+window.Recurrente = Recurrente
+export default Recurrente
